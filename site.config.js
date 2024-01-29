d2cc42cd0537b5c441e96573ff13e35d1e3d9c37
jungjunhyeok <83487028+ted050505@users.noreply.github.com>
2024-01-29 16:54:17 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "jungjunhyeok",
-    image: "/junhyeok.JPG", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/memo_01.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Engineer",
     bio: "",
     email: "ted05050505@gmail.com",
